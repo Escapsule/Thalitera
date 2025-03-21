881869403c028297a80f1452f8c4a4869220c6c4
General_K1ng <General_K1ng@outlook.com>
2025-03-21 13:23:16 +0800

    build(server): Update app configuration and ignore files

--- server/src/main/java/com/escapsule/thalitera/controller/user/UserController.java
+++ server/src/main/java/com/escapsule/thalitera/controller/user/UserController.java
@@ -2,10 +2,7 @@ package com.escapsule.thalitera.controller.user;
 
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * TODO:UserController<br>
- * Login, Register, Update, Delete
- */
+
 @RestController
 public class UserController {
 }
