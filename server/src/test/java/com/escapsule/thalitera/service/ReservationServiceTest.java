7bd061360d9e978711eac6c40903d045c9e5adc9
N1rvana <2324781530@qq.com>
2025-04-29 02:36:53 +0800

    test: several tests

---
+++ server/src/test/java/com/escapsule/thalitera/service/ReservationServiceTest.java
@@ -0,0 +1,40 @@
+package com.escapsule.thalitera.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationServiceTest {
+
+    @Test
+    void getAllActiveMeetingRooms() {
+    }
+
+    @Test
+    void getMeetingRoom() {
+    }
+
+    @Test
+    void makeReservation() {
+    }
+
+    @Test
+    void updateReservation() {
+    }
+
+    @Test
+    void cancelReservation() {
+    }
+
+    @Test
+    void getAllReservations() {
+    }
+
+    @Test
+    void getMyReservations() {
+    }
+
+    @Test
+    void getMeetingRoomReservedTime() {
+    }
+}
